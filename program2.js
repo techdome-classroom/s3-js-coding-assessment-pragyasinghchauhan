64cd1d8900c5722b6fca1c51aac266e7e299b1c1
pragyasinghchauhan <pragyachauhan1902@gmail.com>
2024-09-30 15:52:27 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,26 +1,22 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var romanToInt = function(s) {
-    const romanMap = {
-        'I':1,
-        'V':5,
-        'X':10,
-        'L':50,
-        'C':100,
-        'D':500,
-        'M':1000
+    let res = 0;
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     };
-    let result = 0;
-    for(let i = 0;i<s.length;i++){
-        if(i+1<s.length && romanMap[s[i]]<romanMap[s[i+1]]){
-            result -= romanMap[s[i]];
-        }else{
-            result += romanMap[s[i]];
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            res -= roman[s[i]];
+        } else {
+            res += roman[s[i]];
         }
     }
-    return result;
-    
+
+    return res + roman[s[s.length - 1]];    
 };
-module.exports={romanToInt};
