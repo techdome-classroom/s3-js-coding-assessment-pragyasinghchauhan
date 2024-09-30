efde9a3535f17d4e5946a42b8d3e90e9e13a9e72
pragyasinghchauhan <pragyachauhan1902@gmail.com>
2024-09-30 15:33:19 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -17,6 +17,8 @@ for(let i = 0; i<s.length;i++){
         if(stack.length === 0 || stack.pop() !== bracketMap[char]){
             return false;
         }
+    }else{
+        stack.push(char);
     }
 }
 
