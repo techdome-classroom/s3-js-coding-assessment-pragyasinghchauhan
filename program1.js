2ac62dc238cd2567ac737308844921ae761a1871
pragyasinghchauhan <pragyachauhan1902@gmail.com>
2024-09-30 15:51:34 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,13 +3,13 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = []; // create an empty stack to store opening brackets
+    let stack = [];
     for (let c of s) { 
         if (c === '(' || c === '{' || c === '[') { 
             stack.push(c); 
         } else { 
             if (!stack.length ||  
-                (c === ')' && stack[stack.length - 1] !== '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
+                (c === ')' && stack[stack.length - 1] !== '(') ||
                 (c === '}' && stack[stack.length - 1] !== '{') ||
                 (c === ']' && stack[stack.length - 1] !== '[')) {
                 return false; 
