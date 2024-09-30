f829742e8ac4056eb2310496fc256f79633f8d36
pragyasinghchauhan <pragyachauhan1902@gmail.com>
2024-09-30 15:50:30 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -7,7 +7,7 @@ var isValid = function(s) {
     for (let c of s) { // loop through each character in the string
         if (c === '(' || c === '{' || c === '[') { // if the character is an opening bracket
             stack.push(c); // push it onto the stack
-        } else { // if the character is a closing bracket
+        } else { 
             if (!stack.length ||  
                 (c === ')' && stack[stack.length - 1] !== '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
                 (c === '}' && stack[stack.length - 1] !== '{') ||
