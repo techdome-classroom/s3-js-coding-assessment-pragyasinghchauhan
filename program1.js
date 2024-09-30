960739f6214191b6fbd9312d16466db100da3a5d
pragyasinghchauhan <pragyachauhan1902@gmail.com>
2024-09-30 15:49:57 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,26 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const stack = [];
-    const bracketMap = {
-        ')':'(',
-        ']':'[',
-        '}':'{'
-    }
-};
-for(let i = 0; i<s.length;i++){
-    const char = s[i];
-
-    if(char in bracketMap){
-        if(stack.length === 0 || stack.pop() !== bracketMap[char]){
-            return false;
+    let stack = []; // create an empty stack to store opening brackets
+    for (let c of s) { // loop through each character in the string
+        if (c === '(' || c === '{' || c === '[') { // if the character is an opening bracket
+            stack.push(c); // push it onto the stack
+        } else { // if the character is a closing bracket
+            if (!stack.length || // if the stack is empty or 
+                (c === ')' && stack[stack.length - 1] !== '(') || // the closing bracket doesn't match the corresponding opening bracket at the top of the stack
+                (c === '}' && stack[stack.length - 1] !== '{') ||
+                (c === ']' && stack[stack.length - 1] !== '[')) {
+                return false; // the string is not valid, so return false
+            }
+            stack.pop(); // otherwise, pop the opening bracket from the stack
         }
-    }else{
-        stack.push(char);
     }
-}
-return stack.length === 0;
-
-module.exports = { isValid };
+    return !stack.length; // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+                          // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+};
 
 
